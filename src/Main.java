7314b8270e07c5c5adec986646e856bdd8b12869
Homework3 <tradervdovin@gmail.com>
2023-01-01 13:42:19 +0500

    Решение до задачи 6

---
+++ src/Main.java
@@ -0,0 +1,103 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // Задача 1
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ",то он совершеннолетний");
+        }
+        if (age <18) {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия и нужно немного подождать");
+        }
+        // Задача 2 без Else
+        int temperature = 6;
+        if (temperature > 5) {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+        if (temperature < 5) {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+
+        // Задача 2 с Else
+        int temperature2 = 4;
+        if (temperature2 >5) {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        } else {
+            System.out.println("На улице холодно, нужно надеть шапку");
+        }
+        System.out.println("На улице " + temperature2 + " градуса, нужно надеть шапку");
+
+        // Задача 3
+        int speed = 61;
+        if (speed <60) {
+            System.out.println("Можно ездить спокойно");
+        }
+        if (speed >60) {
+            System.out.println("Придётся заплатить штраф");
+        }
+        System.out.println("Если скорость " + speed + ", то придётся заплатить штраф");
+
+        int speed2 = 59;
+        if (speed2 <60) {
+            System.out.println("Можно ездить спокойно");
+        } else {
+            System.out.println("Придётся заплатить штраф");
+        }
+        System.out.println("Если скорость " + speed2 + ", то можно ездить спокойно");
+
+        // Задача 4
+        int newage = 25;
+        if (newage >= 2 && newage <= 6) {
+            System.out.println("Нужно ходить в детский сад");
+        }
+        if (newage >= 7 && newage <=18) {
+            System.out.println("Нужно ходить в школу");
+        }
+        if (newage > 18 && newage < 24) {
+            System.out.println("Нужно ходить в университет");
+        }
+        if (newage >=24) {
+            System.out.println("Нужно ходить на работу");
+        }
+        System.out.println("Если возраст человека равен " + newage + ",то ему нужно ходить на работу ");
+
+        // Задача 5
+        int ages = 4;
+        if (ages < 5) {
+            System.out.println("Нельзя кататься на аттракционе");
+        }
+        if (ages > 5 && ages < 14) {
+            System.out.println("Можно кататься в сопровождении");
+        }
+        if (ages > 14) {
+            System.out.println("Можно кататься без сопровождения");
+        }
+        System.out.println("Если возраст ребёнка равен " + ages + ",то ему нельзя кататься на аттракционе");
+
+        // Задача 5 с else
+        int vozrast = 17;
+        if (vozrast < 5 || vozrast > 5 && vozrast <14) {
+            System.out.println("Нельзя кататься на аттракционе");
+        } else {
+            System.out.println("Можно кататься в сопровождении");
+        }
+        if (vozrast > 14) {
+            System.out.println("Можно кататься без сопровождения");
+        }
+        System.out.println("Если возраст ребёнка равен " + vozrast + ",то ему можно кататься на аттракционе без сопровождения");
+
+        // Задача 6
+        int Totalseats = 102;
+
+
+
+
+
+
+
+
+    }
+}
